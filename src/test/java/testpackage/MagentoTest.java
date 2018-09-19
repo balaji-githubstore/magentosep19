2c6d00b32c582f8c1fe590094b76cd5ebed76db4
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-09-19 06:28:04 +0530

    fcommit

---
+++ src/test/java/testpackage/MagentoTest.java
@@ -0,0 +1,35 @@
+package testpackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import magetnopages.HomePage;
+public class MagentoTest {
+	@Test
+	public  void main() {
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://magento.com");
+		System.out.println(driver);
+		
+		HomePage home=new HomePage(driver);
+
+		home.clickOnMyAccount();
+
+/*		LoginPage.sendUserName(driver, "balajidinakaran1@gmail.com");
+		LoginPage.sendPassword(driver, "Welcome123");
+		LoginPage.clickOnLogin(driver);
+
+		String title = MainPage.getCurrentTitle(driver, wait);
+		System.out.println(title);
+
+		MainPage.clickOnLogOut(driver);*/
+
+	}
+}
